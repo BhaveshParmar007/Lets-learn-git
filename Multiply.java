f526183ab1c20ee5b5d4950f545cc6824b1fd492
Bhavesh <Bhaveshparmarhprx77@gmail.com>
2021-10-08 13:19:42 +0530

    This is the file we made in a new branch

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+package learngit.day3;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a =45;
+        int b = 23;
+        System.out.println(a*b);
+
+    }
+    
+}
