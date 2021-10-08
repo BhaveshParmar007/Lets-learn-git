324a1e45780d80599ac2642454d95fa8d4e22f2c
Bhavesh <Bhaveshparmarhprx77@gmail.com>
2021-10-08 07:53:43 +0530

    this is you commit 1 of day 3

---
+++ sum2.java
@@ -0,0 +1,13 @@
+package learngit.day3;
+
+public class sum2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = a+b;
+        System.out.println(sum);
+        System.out.println("hello there");
+
+    }
+    
+}
