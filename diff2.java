3637d32a31bc917d81f90029a00b1683a3b6850b
Bhavesh <Bhaveshparmarhprx77@gmail.com>
2021-10-08 07:54:41 +0530

    this is meessage printed at last because u don't use -m with git.

---
+++ diff2.java
@@ -0,0 +1,11 @@
+package learngit.day3;
+
+public class diff2 {
+    public static void main(String[] args) {
+        int a = 64;
+    int b= 32;
+    int diff = a-b;
+    //System.out.println(diff2);
+  System.out.println(diff);
+    }
+}
